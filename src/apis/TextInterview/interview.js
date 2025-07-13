a1dcca2937ec4f8f06554de403697285fa979f9d
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-07-13 11:04:17 +0530

    feat: implement interview question and response fetching functions

---
+++ src/apis/TextInterview/interview.js
@@ -0,0 +1,21 @@
+import axiosConfig from "@/config/axiosConfig";
+
+export async function fetchInterviewQuestion(data) {
+    try {
+        const response = await axiosConfig.post("/interview/question", data);
+        return response.data;
+    } catch (error) {
+        console.log("error in fetchInterviewQuestion:", error);
+        throw error;
+    }
+}
+
+export async function fetchInterviewResponse(data) {
+    try {
+        const response = await axiosConfig.post("/interview/response", data);
+        return response.data;
+    } catch (error) {
+        console.log("error in fetchInterviewResult:", error);
+        throw error;
+    }
+}
