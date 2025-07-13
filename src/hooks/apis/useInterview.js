0e521e8aab083e71d7ee897e382213226107e1b5
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-07-13 11:04:24 +0530

    feat: add custom hooks for fetching interview questions and responses

---
+++ src/hooks/apis/useInterview.js
@@ -0,0 +1,40 @@
+import {
+  fetchInterviewQuestion,
+  fetchInterviewResponse,
+} from '@/apis/TextInterview/interview';
+import { useMutation } from "@tanstack/react-query";
+
+export const useInterviewQuestion = () => {
+    const {
+      isPending,
+      isSuccess,
+      error,
+      mutateAsync: fetchInterviewQuestionMutation,
+    } = useMutation({
+      mutationFn: (data) => fetchInterviewQuestion(data),
+      onSuccess: (data) => {},
+      onError: (error) => {
+        console.error('Error fetching interview question:', error);
+      },
+    });
+
+    return { isPending, isSuccess, error, fetchInterviewQuestionMutation}
+}
+
+
+export const useInterviewResponse = () => {
+    const {
+      isPending,
+      isSuccess,
+      error,
+      mutateAsync: fetchInterviewResponseMutation,
+    } = useMutation({
+      mutationFn: (data) => fetchInterviewResponse(data),
+      onSuccess: (data) => {},
+      onError: (error) => {
+        console.error('Error fetching interview response:', error);
+      },
+    });
+
+    return { isPending, isSuccess, error, fetchInterviewResponseMutation }
+}
